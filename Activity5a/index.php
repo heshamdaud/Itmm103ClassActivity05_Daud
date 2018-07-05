7b45513200ac8981053072fd8f8297f39e89571a
Hesham Daud <heshamdauditmm103@gmail.com>
2018-07-05 11:35:07 +0800

    Fruit Activity

---
+++ Activity5a/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+$Fruit1 = array("Mango","Mangifera indica",);
+$Fruit2 = array("test","test2");
+$Fruit3 = array(", array2)
+foreach ($array as $value) {
+    if ($value === 'one') {
+       array_push($array2, $value);
+    }
+}
+
+print_r($array2);
+</body>
+</html>
